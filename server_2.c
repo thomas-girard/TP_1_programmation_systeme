164d1ae6bf6b13cfb2f419ee0ec8e21123f44b44
thomas-girard <t.girard912@laposte.net>
2021-10-06 09:16:33 +0200

    petite correction sur 1.2

--- server_2.c
+++ server_2.c
@@ -11,13 +11,23 @@
 // kill avec le PID du père sans option n'affiche pas le message
 // kill avec le PID du père avec l'option "-s KILL" fait crasher la fenêtre du bash
 // il est impossible d'afficher un message avec -s KILL
-//sans modifier la variable running : ctr + c tue le programme // kill sans option tue le programme // kill -9 tue le programme sans message de fin
+//sans modifier la variable running : ctr + c tue le programme au bout de la 2ème fois// kill sans option ne tue pas le programme // kill -9 tue le programme sans message de fin
+
+// Question 1.3 :
+// avec ctr + c : fonction atexit activée
+// avec kill : fonction atexit activée
+// avec kill -9 : fonction atexit pas activée
+
 
 int running = 1;
 
 void stop_handler(int sig) {
     printf("signal reçu : %i  \n", sig);
-    running = 0;
+    //running = 0;
+}
+
+void exit_message() {
+    printf("message de exit_message \n");
 }
 
 int main() {
@@ -26,6 +36,8 @@ int main() {
     action.sa_handler = stop_handler;
     sigaction(SIGINT, &action, NULL);
     sigaction(SIGTERM, &action, NULL);
+
+    int cr = atexit( exit_message );
 
     while(running == 1) {
         printf("hello world \n");
